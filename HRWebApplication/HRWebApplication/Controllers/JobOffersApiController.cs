2a3659b8dacaa5422e7fede5ec089b58ef9ba4e1
Rogal27 <Rogal27@users.noreply.github.com>
2020-01-03 23:11:37 +0100

    #55 filtering works, its using ajax

--- HRWebApplication/HRWebApplication/Controllers/JobOffersApiController.cs
+++ HRWebApplication/HRWebApplication/Controllers/JobOffersApiController.cs
@@ -23,15 +23,9 @@ namespace HRWebApplication.Controllers
 
         // GET: api/JobOffersApi
         [HttpGet]
-        public async Task<PagingViewModel<JobOffers>> GetJobOffers(int? pageNumber = 1, int? pageSize = 10)
+        public async Task<PagingViewModel<JobOffers>> GetJobOffers(int pageNumber = 1, int pageSize = 10, string searchString = "", int? companyId = null, string location = null, int? salaryFrom = 0, int? salaryTo = int.MaxValue, string sortOrder = null)
         {
-            //return await _context.JobOffers.Include(j => j.JobOfferStatus).ToListAsync();
-
-            var hRProjectDatabaseContext = _context.JobOffers.Include(j => j.Company).Include(j => j.JobOfferStatus).OrderByDescending(j => j.CreationDate);
-            if (pageSize.HasValue == false)
-            {
-                pageSize = 10;
-            }
+            var result = _context.JobOffers.Include(j => j.Company).Include(j => j.JobOfferStatus);
             if (pageSize < 1)
             {
                 pageSize = 10;
@@ -40,13 +34,89 @@ namespace HRWebApplication.Controllers
             {
                 pageNumber = 1;
             }
-            int totalPage, totalRecord;
+            if (salaryFrom.HasValue == false)
+            {
+                salaryFrom = 0;
+            }
+            if(salaryTo.HasValue == false)
+            {
+                salaryTo = int.MaxValue;
+            }
+            if (searchString == null)
+            {
+                searchString = "";
+            }
+            if (salaryFrom < 0)
+            {
+                salaryFrom = 0;
+            }
+            if (salaryTo < salaryFrom)
+            {
+                salaryTo = salaryFrom;
+            }
+            if(sortOrder == null)
+            {
+                sortOrder = "";
+            }
 
-            totalRecord = await hRProjectDatabaseContext.CountAsync();
+            var result_filter = result
+                .Where(p => p.JobTitle.ToLower().Contains(searchString.ToLower()))
+                .Where(p => p.SalaryFrom.HasValue == false || p.SalaryFrom.Value >= salaryFrom)
+                .Where(p => p.SalaryTo.HasValue == false || p.SalaryTo.Value <= salaryTo);
+
+            if (location != null && companyId != null)
+            {
+                result_filter = result_filter.Where(p => p.Location == location).Where(p => p.CompanyId == companyId);
+            }
+            else if (location != null)
+            {
+                result_filter = result_filter.Where(p => p.Location == location);
+            }
+            else if (companyId != null)
+            {
+                result_filter = result_filter.Where(p => p.CompanyId == companyId);
+            }       
+            
+            switch(sortOrder)
+            {
+                case "title_desc":
+                    result_filter = result_filter.OrderByDescending(j => j.JobTitle).ThenByDescending(j => j.CreationDate);
+                    break;
+                case "title_asc":
+                    result_filter = result_filter.OrderBy(j => j.JobTitle).ThenByDescending(j => j.CreationDate);
+                    break;
+                case "company_desc":
+                    result_filter = result_filter.OrderByDescending(j => j.Company.CompanyName).ThenByDescending(j => j.CreationDate);
+                    break;
+                case "company_asc":
+                    result_filter = result_filter.OrderBy(j => j.Company.CompanyName).ThenByDescending(j => j.CreationDate);
+                    break;
+                case "location_desc":
+                    result_filter = result_filter.OrderByDescending(j => j.Location).ThenByDescending(j => j.CreationDate);
+                    break;
+                case "location_asc":
+                    result_filter = result_filter.OrderBy(j => j.Location).ThenByDescending(j => j.CreationDate);
+                    break;
+                case "salary_desc":
+                    result_filter = result_filter.OrderByDescending(j => j.SalaryFrom).ThenByDescending(j => j.CreationDate);
+                    break;
+                case "salary_asc":
+                    result_filter = result_filter.OrderBy(j => j.SalaryFrom).ThenByDescending(j => j.CreationDate);
+                    break;
+                case "date_asc":
+                    result_filter = result_filter.OrderBy(j => j.CreationDate);
+                    break;
+                default:
+                case "date_desc":
+                    result_filter = result_filter.OrderByDescending(j => j.CreationDate);
+                    break;
+            }
+
+            int totalRecord = await result_filter.CountAsync();
             //totalPage = (totalRecord / pageSize.Value) + ((totalRecord % pageSize.Value) > 0 ? 1 : 0);
-            var record = await hRProjectDatabaseContext.Skip((pageNumber.Value - 1) * pageSize.Value).Take(pageSize.Value).ToListAsync();
+            var record = await result_filter.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
 
-            PagingViewModel<JobOffers> data = new PagingViewModel<JobOffers>(record, totalRecord, pageSize.Value);
+            PagingViewModel<JobOffers> data = new PagingViewModel<JobOffers>(record, totalRecord, pageSize);
             return data;
             //return await PaginatedList<JobOffers>.CreateAsync(hRProjectDatabaseContext.AsNoTracking(), pageNumber ?? 1, pageSize.Value);
         }
@@ -139,6 +209,21 @@ namespace HRWebApplication.Controllers
             await _context.SaveChangesAsync();
 
             return Ok(jobOffers);
+        }
+
+        [Route("[action]")]
+        [HttpGet]
+        public async Task<IEnumerable<string>> GetLocations()
+        {
+            var data = await _context.JobOffers.Select(s => s.Location).Distinct().OrderBy(s => s).ToListAsync();
+            return data;
+        }
+        [Route("[action]")]
+        [HttpGet]
+        public async Task<IEnumerable<Companies>> GetCompanies()
+        {
+            var data = await _context.Companies.OrderBy(c => c.CompanyName).ToListAsync();
+            return data;
         }
 
         private bool JobOffersExists(int id)
